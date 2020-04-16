39a18edbc309861135a4b07d9f9190203823c214
hasanmertmercan <mercan.hasanmert@gmail.com>
2020-04-16 22:22:43 +0200

    implemented question generator

---
+++ MathTestGenerator/Data/Question.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MathTestGenerator.Data
+{
+    public class Question
+    {
+        public int[] NumbersToUse { get; set; } = new int[2];
+        public Operations Operation { get; set; }
+        public int Result { get; set; }
+    }
+
+    public enum Operations
+    {
+        ADDITION = 0,
+        EXTRACTION = 1,
+        MULTIPLICATION = 2,
+        DIVISION = 3
+    }
+}
